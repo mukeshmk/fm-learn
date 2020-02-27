5a3b1e039a433b89592ce7d263bb25eeada6e902
Mukesh A <amukesh.mk@gmail.com>
2020-02-27 01:37:49 +0000

    added config and constants file

---
+++ config.py
@@ -0,0 +1,4 @@
+ENV = 'prod'
+LOCAL_SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:admin@localhost/testdb01'
+SERVER_SQLALCHEMY_DATABASE_URI = 'postgres://zekyfooodqjhyo:6922052eb84615083581f2368c6485388a387a5aac6084cfe77c6155e2b55494@ec2-54-195-247-108.eu-west-1.compute.amazonaws.com:5432/d6c5dgaj4odfug'
+SQLALCHEMY_TRACK_MODIFICATIONS = False
---
+++ constants.py
@@ -0,0 +1,23 @@
+# Table Names
+TABLE_METRIC = 'metric'
+TABLE_PARAM = 'params'
+
+# Table Class Names
+CLASS_METRIC = 'Metric'
+CLASS_PARAM = 'Params'
+
+# Rest Methods
+GET = 'GET'
+POST = 'POST'
+DEL = 'DELETE'
+PUT = 'PUT'
+
+# Rest API Constants
+METRIC = '/metric'
+VAR_ID = '/<id>'
+RETRIEVE = '/retrieve'
+ALL = '/all'
+MAX = '/max'
+MIN = '/min'
+
+
