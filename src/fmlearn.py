446d4261e093eaf62e153f2b954d475bd4ee22b7
Mukesh A <amukesh.mk@gmail.com>
2020-06-17 01:06:43 +0100

    added TODOs and test function to fmlearn class

--- src/fmlearn.py
+++ src/fmlearn.py
@@ -22,6 +22,8 @@ class fmlearn:
         self._accuracy = None
 
     def load_data(self):
+        # TODO: force new model to be trained once data has been reloaded?
+
         # loads data from the SQL database and pre-processes the data.
         self._df = utils.get_df_from_db()
         self._shape = self._df.shape
@@ -37,6 +39,8 @@ class fmlearn:
         self._y, _ = utils.label_encode_feature(self._y, utils.DATASET_HASH)
 
     def train(self):
+        # TODO: throw error if the data is not loaded before training.
+        # remove the below work around.
         if self._X is None:
             print('data not loaded! \nloading data and then training model')
             self.load_data()
@@ -51,37 +55,53 @@ class fmlearn:
         y_pred = self._model.predict(X_test)
         self._accuracy = accuracy_score(y_test, y_pred)
 
+    def load_data_and_train(self):
+        self.load_data()
+        self.train()
+
     def predict(self, X_pred):
+        # TODO: check if the shape of the input df matches that used to train the model.
+        # TODO: force retrain of model if the model is older than a set time frame?
+
         y_pred = self._model.predict(X_pred)
+
+        # TODO: return either the proper dataset_hash or
+        # the algorithm used by that dataset which is predicted.
+
         return y_pred
+
+    def _test(self, print_details=False):
+        # this function tests the entire functionality without affecting the class variables
+        # loads data from db, pre-processes it and trains a model and displays the 
         
+        df = utils.get_df_from_db()
+        df.fillna(-1, inplace=True)
 
+        X, y = utils.get_Xy(df)
 
+        # pre processing of data
+        X, _ = utils.ohe_feature(X, utils.TARGET_TYPE)
 
-def kmc():
-    df = utils.get_df_from_db()
-    df.fillna(-1, inplace=True)
+        y, _ = utils.label_encode_feature(y, utils.DATASET_HASH)
 
-    X, y = utils.get_Xy(df)
+        # train test split
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=123)
 
-    # pre processing of data
-    X, _ = utils.ohe_feature(X, utils.TARGET_TYPE)
+        model = KNeighborsClassifier()
 
-    y, _ = utils.label_encode_feature(y, utils.DATASET_HASH)
+        model.fit(X_train, y_train)
+        
+        y_pred = model.predict(X_test)
 
-    # train test split
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=123)
+        print('accuracy: ' + str(accuracy_score(y_test, y_pred)))
 
-    model = KNeighborsClassifier()
-
-    model.fit(X_train, y_train)
-    
-    y_pred = model.predict(X_test)
-
-    print(model.kneighbors(X_test)[1])
-    print(y_test.to_string(header=False))
-    y_pred = pd.DataFrame(y_pred)
-    return y_pred.to_string(header=False)
+        if print_details:
+            # kneighbors()[0] contains distances to points
+            # kneighbors()[1] contains indcies of nearest points
+            print(model.kneighbors(X_test)[1])
+            
+            print(y_test.to_string(header=False))
+            print(pd.DataFrame(y_pred).to_string(header=False))
 
 if __name__ == "__main__":
     kmc()
