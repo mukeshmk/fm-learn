fe1d8b4525bf7f16de33b8112b12f3d08897a38f
Mukesh A <amukesh.mk@gmail.com>
2020-06-19 02:04:39 +0100

    WIP: added test api for fmlearn predict()

--- src/api.py
+++ src/api.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, Flask, request, jsonify
+import pandas as pd
 
 from src.utils.constants import *
 
@@ -9,6 +10,17 @@ from src.data_models.Params import *
 from src.data_models.MetaFeatures import *
 
 from app import db
+
+from src.fmlearn import fmlearn
+from src.utils import utils
+
+# global FMLearn object which is to be used across the application
+# for algorithm predection (at least that's the plan)
+fml = fmlearn()
+
+# doing this so that dataload and training of the model happens once at application start.
+fml.load_data()
+fml.train()
 
 # Create a Metric
 @metrics_api.route('', methods=[POST])
@@ -112,3 +124,49 @@ def retrieve_algorithm_best_max():
     metric = Metric.query.filter_by(dataset_hash=dataset_hash).order_by(Metric.metric_value.desc()).first()
 
     return metric_schema.jsonify(metric)
+
+# test API for fmlearn
+# TODO: imporove performance? seems way too overcomplicated
+@metrics_api.route('test', methods=[GET])
+def test_fmlearn():
+    dataset_hash = request.json[DATASET_HASH].replace("\x00", "")
+    target_type = request.json[TARGET_TYPE]
+    data_meta_features = request.json[META_FEATURES]
+
+    data = {}
+    
+    # fetching the encoder for target type for encoding the input data
+    tt_encoder = fml.get_encoders()[utils.TARGET_TYPE]
+    # fetching the columns used in the dataframe for the said encoder
+    tt_cols = [utils.TARGET_TYPE + ': ' + str(i.strip('x0123_')) for i in tt_encoder.get_feature_names()]
+
+    # creating a dataframe of target type to endode
+    tt = pd.DataFrame([str(target_type)])
+    
+    # creating a dataframe after encoding with the appropriate columns names
+    tt = pd.DataFrame(tt_encoder.transform(tt), columns = tt_cols)
+    # removing the last columns in the case of OHE
+    tt = tt.iloc[:, :-1]
+
+    # creating a dataframe with all cols the X df
+    df = pd.DataFrame(columns = fml.get_X_cols())
+
+    # converting the json into a dataframe which can be used as input to predict()
+    if(data_meta_features != ""):
+        for feat in data_meta_features:
+            if feat[FEAT_NAME] not in data:
+                data[feat[FEAT_NAME]] = []
+            data[feat[FEAT_NAME]].append(float(feat[FEAT_VALUE]))
+
+    df = df.append(pd.DataFrame.from_dict(data))
+
+    # merging target type encoded columns with the final df
+    for i in range(len(tt_cols) - 1):
+            df[tt_cols[i]] = tt[tt_cols[i]]
+
+    # replacing NA values in the dataframe with -1
+    # NA values are possible because the shape of df is difference for regression and classification probs
+    # and since `fml.get_X_cols()` ensures that all columns are used for the df there is a possibility of NA values
+    df.fillna(-1, inplace=True)
+
+    return str(fml.predict(df))
